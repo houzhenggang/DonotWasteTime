e38159a38f089bb98d16d17746c4150c9e9e40e2
xulei <xulei>
2016-04-18 21:41:10 +0800

    新建WinForm程序，使用WMI可以监控系统新启动的进程

---
+++ DonotWasteTime.BLL/WasteTimeMonitor.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Management;
+
+
+namespace DonotWasteTime.BLL
+{ 
+    /// <summary>
+    /// 监控是否有浪费时间的进程
+    /// </summary>
+    public class WasteTimeMonitor
+    {
+        public ManagementEventWatcher mgmtWtch;
+
+        //public event Warn;
+
+        /// <summary>
+        /// 监控
+        /// </summary>
+        public void Monitoring()
+        {
+            mgmtWtch = new ManagementEventWatcher("Select * From Win32_ProcessStartTrace");
+            mgmtWtch.EventArrived += new EventArrivedEventHandler(mgmtWtch_EventArrived);
+            mgmtWtch.Start();
+        }
+
+        private void mgmtWtch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine((string)e.NewEvent["ProcessName"]);
+        }
+
+        private void CheckingBlackListEvent(object sender, EventArrivedEventArgs e)
+        {
+            var blackProcessSet = this.GetWasteTimeProcess();
+            if (blackProcessSet.Contains((string)e.NewEvent["PrcessName"]))
+            {
+
+            }
+        }
+
+
+        /// <summary>
+        /// 获得 受限进程列表  
+        /// </summary>
+        /// <returns></returns>
+        private HashSet<string> GetWasteTimeProcess()
+        {
+            var set = new HashSet<string>();
+            set.Add("11Game.exe");
+            return set;
+        }
+
+    }
+}
---
+++ DonotWasteTime/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DonotWasteTime
+{
+    public partial class Form1 : Form
+    {
+        public System.Management.ManagementEventWatcher mgmtWtch;
+        public HashSet<string> blackSet;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            mgmtWtch = new System.Management.ManagementEventWatcher("Select * From Win32_ProcessStartTrace");
+            mgmtWtch.EventArrived += new System.Management.EventArrivedEventHandler(CheckingBlackListEvent);
+            mgmtWtch.Start();
+        }
+
+        void mgmtWtch_EventArrived(object sender, System.Management.EventArrivedEventArgs e)
+        {
+            MessageBox.Show((string)e.NewEvent["ProcessName"]);
+        }
+
+        private void CheckingBlackListEvent(object sender, System.Management.EventArrivedEventArgs e)
+        {
+            var prcessName = (string)e.NewEvent["ProcessName"];
+            MessageBox.Show(prcessName);
+            if (this.blackSet == null)
+            {
+                this.LoadWasteTimeProcess();
+            }
+            
+            if (this.blackSet.Contains(prcessName))
+            {
+                MessageBox.Show("你能通过游戏赚钱吗！不能还玩什么！");
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mgmtWtch.Stop();
+        }
+
+
+        /// <summary>
+        /// 获得 受限进程列表  
+        /// </summary>
+        /// <returns></returns>
+        public void LoadWasteTimeProcess()
+        {
+            MessageBox.Show("加载黑名单列表");
+            this.blackSet = new HashSet<string>();
+            this.blackSet.Add("11Game.exe");
+          
+        }
+    }
+}
